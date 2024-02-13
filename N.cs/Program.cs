14f0ed6deca8503b905aff211480584207f5a670
JacobMarkwell <52938992+JacobMarkwell@users.noreply.github.com>
2024-02-12 21:11:31 -0600

    Update Program.cs

--- N.cs/Program.cs
+++ N.cs/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the value of N:");
+        int N = int.Parse(Console.ReadLine());
+
+        if (N <= 0)
+        {
+            Console.WriteLine("N must be a positive integer.");
+            return;
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (i == 0 || i == N - 1 || j == 0 || j == N - 1)
+                {
+                    Console.Write("* ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
